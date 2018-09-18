40c3391c2c6641fb2a2673b0cc6cd20608fb48a7
porcelain <982503660@qq.com>
2018-09-19 00:44:56 +0800

    update: observer/array.js

--- core/observer/array.js
+++ core/observer/array.js
@@ -5,9 +5,11 @@
 
 import { def } from '../util/index'
 
+// 使用Array.prototype创建新的数组对象, 避免污染Array.prototype上的方法
 const arrayProto = Array.prototype
 export const arrayMethods = Object.create(arrayProto)
 
+// 对这些数组方法进行重写, 功能与原方法一致, 只是新增了一步通知watcher相应变化的操作
 const methodsToPatch = [
   'push',
   'pop',
@@ -23,11 +25,13 @@ const methodsToPatch = [
  */
 methodsToPatch.forEach(function (method) {
   // cache original method
+  // 缓存数组原生方法, 以便后续调用, 用于重写方法时保持原功能不变
   const original = arrayProto[method]
   def(arrayMethods, method, function mutator (...args) {
     const result = original.apply(this, args)
     const ob = this.__ob__
     let inserted
+    // 这三个操作是插入操作, 需要额外的observeArray方法进行观察变更
     switch (method) {
       case 'push':
       case 'unshift':
@@ -39,6 +43,7 @@ methodsToPatch.forEach(function (method) {
     }
     if (inserted) ob.observeArray(inserted)
     // notify change
+    // 将改动通知watcher
     ob.dep.notify()
     return result
   })
