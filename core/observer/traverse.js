b4f63753389f1fbe5feebe0051b5fbd7cdc77075
wujinze <wujinze@hellobike.com>
2018-09-19 12:44:03 +0800

    update: observer/traverse.js

--- core/observer/traverse.js
+++ core/observer/traverse.js
@@ -11,6 +11,7 @@ const seenObjects = new Set()
  * getters, so that every nested property inside the object
  * is collected as a "deep" dependency.
  */
+// 对数组和对象进行递归遍历, 对每个属性都进行依赖收集
 export function traverse (val: any) {
   _traverse(val, seenObjects)
   seenObjects.clear()
@@ -19,6 +20,7 @@ export function traverse (val: any) {
 function _traverse (val: any, seen: SimpleSet) {
   let i, keys
   const isA = Array.isArray(val)
+  // 三种情况不进行递归处理, 1.不是数组或对象, 2.是一个冻结对象, 3. 是一个vnode
   if ((!isA && !isObject(val)) || Object.isFrozen(val) || val instanceof VNode) {
     return
   }
@@ -29,6 +31,7 @@ function _traverse (val: any, seen: SimpleSet) {
     }
     seen.add(depId)
   }
+  // 递归处理数组和对象
   if (isA) {
     i = val.length
     while (i--) _traverse(val[i], seen)
